00f42e1263e23f990afd237b3d44095c47a367c3
MilosJovic23 <milos.mj992@gmail.com>
2024-11-13 11:35:17 +0100

    * added logic for sessions homework ( predavanje17 )

---
+++ modeli/logout.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+
+
+    $_SESSION['logged'] = false;
+
+    session_destroy();
+
+    header('Location: ../products.php');
